b5aceb3152d2369cb8b93ce878653d3298355929
ooothinhooo <74719505+ooothinhooo@users.noreply.github.com>
2023-06-24 14:54:57 +0700

    update

--- client/src/API/Api/FindRoomBill.api.js
+++ client/src/API/Api/FindRoomBill.api.js
@@ -4,8 +4,8 @@ import { URL_DEPLOP, URL_LIST_ROOM } from "../index.js";
 export function FIND_BILL(roomId, month) {
   return axios({
     method: "get",
-    // url: `${URL_DEPLOP}/api/findroombill`,
-    url: "http://localhost:9000/api/findroombill",
+    url: `${URL_DEPLOP}/api/findroombill`,
+    // url: "http://localhost:9000/api/findroombill",
 
     params: {
       roomId: roomId,
